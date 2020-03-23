fb7a2e4310fb5818595751ea62ccf42188cfd0c7
Glitch (glitchchord) <none>
2020-03-23 13:44:35 +0000

    🔋🗃 Checkpoint ./views/register.ejs:5152800/2299 ./app.js:5152800/26

--- app.js
+++ app.js
@@ -59,14 +59,14 @@ app.use(passport.session());
 
 // Login Strategy
 passport.use("local-login", new LocalStrategy({
-    usernameField: "email",
+    usernameField: "username",
     passwordField: "password",
     passReqToCallback: true,
 }, passportStrategy.localSiginStrategy));
 
 // Sign UP Strategy
 passport.use("local-signup", new LocalStrategy({
-    usernameField: "email",
+    usernameField: "username",
     passwordField: "password",
     passReqToCallback: true,
 }, passportStrategy.localSignupStrategy));
