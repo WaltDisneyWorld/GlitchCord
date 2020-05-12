022e19ff3107c201ccf280b9e247050099b8117c
Glitch (glitchchord) <none>
2020-05-12 17:41:56 +0000

    Rewound to commit 790351d0699f14846e73204ccee77abeefc9155c + 0 edits

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ mongoose.Promise = global.Promise;
 
 
 // boot if db is available
-mongoose.connect(config.dbURL, { reconnectTries: 10 })
+mongoose.connect(config.dbURL, { reconnectTries: 5 })
     .then(()=>{
         // boot
         server.listen(config.port, ()=>{
