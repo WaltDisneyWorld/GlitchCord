8d581bc74328dd2da0c65aa54eec06b88b1ad9b1
Glitch (glitchchord) <none>
2020-05-17 15:50:22 +0000

    🍏🐜 Checkpoint ./routes/index.js:5152800/612 ./views/partials/header.ejs:5152800/310

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,6 @@ const User        = require("../models/user");
 const router      = express.Router();
 
 router.get("/", (req, res)=>{
-    res.cookie('', 'john doe', { maxAge: 900000, httpOnly: true });
     if(req.user){
         User.findById(req.user._id).then(()=>res.redirect("/users/@me"))
         .catch((e)=>{
