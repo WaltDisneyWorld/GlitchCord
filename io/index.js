c14cc0d2d36665fcc5d9f9560cfe41b6ecd2f691
Glitch (glitchchord) <none>
2020-07-07 18:57:40 +0000

    🏕🤗 Checkpoint ./io/index.js:1025089/1537 ./public/js/chat.js:1025089/8323:5152800/23 ./app.js:5152800/2

--- io/index.js
+++ io/index.js
@@ -1,44 +1,21 @@
 const { saveMessage } = require("../io/utils");
 
+module.exports = io => {
+  io.on("connection", socket => {
+    console.log("New User Connected");
 
-module.exports = (io)=>{
-      
-        io.on("connection", (socket)=>{
-         console.log("New User Connected");
-
-
-        socket.on("join", (params, callback)=>{
-            socket.join(params.channelID);
-            callback();
-        });
-
-        socket.on("createdMessage", (data, callback)=>{
-            saveMessage(io, data);
-            callback();
-        });
-
-        socket.on("disconnect", ()=>{
-             console.log("Diconected");
-        });
+    socket.on("join", (params, callback) => {
+      socket.join(params.channelID);
+      callback();
     });
-const commands = {
-  shrug = () => sendMessage("¯\\_(ツ)_/¯"),
-  help => () => renderMessage("System", "/shrug - Send a shrug ascii text.")
-};
 
-function sendMessage (message)
-{
-  if (message.startsWith("/"))
-  {
-    const args = message.substring(1, message.length).trim().split(/\s/g);
-    const command = args.shift().toLowerCase();
-    if (!command) return renderError("Missing command input!");
-    if (!commands[command]) return renderError("Invalid command!");
-    commands[command](args);
-  } else
-  {
-    socket.emit("message", message);
-  }
-}
-[  
+    socket.on("createdMessage", (data, callback) => {
+      saveMessage(io, data);
+      callback();
+    });
+
+    socket.on("disconnect", () => {
+      console.log("Diconected");
+    });
+  });
 };
--- public/js/chat.js
+++ public/js/chat.js
@@ -3,54 +3,87 @@ const socket = io();
 const chatList = $("#chat-list ul");
 const username = $("#chat-list a");
 
-socket.on("connect", function(){
-    console.log("Connected");
-
-        const params = {
-            channelID,
-            userID
-        };
-
-
-    socket.emit("join", params, function(err){
-        if(err){
-            alert(err);
-            window.location.href = '/';
-        }else{
-            console.log("No Error");
-        }
-    });
-
-});
-
-jQuery("#message-form").on("submit", function(e){
-    e.preventDefault();
-
-    var messageTextBox =  jQuery("[name=message]");
-
-
-    var message = messageTextBox.val().trim();
-  
-    if(message === "") return;
-
-    var data = {
-        userID,
-        channelID : channelID,
-        message
-    };
-
-
-    socket.emit('createdMessage', data, function(){
-        messageTextBox.val(" ");
-    });
-});
-
-socket.on("newMessage", function(message){
-
-    const formatedTime = moment(message.created_at).format("lll");
-
+const commands = {
+  shrug: args => socket.emit("newMessage", { userID, channelID, message: "¯\_(ツ)_/¯" }),
+  help: args => {
     const div = jQuery("<div class='chat-message'></div>");
-    div.html(`
+    div.html(`<div class="chat-message-content">
+      <a href="#">System</div>
+      <div class="chat-message-message">
+        <p><code>/shrug</code> - Send a shrug face in the chat.</p>
+      </div>
+    `);
+    jQuery("#mCSB_2_container").append(div);
+    scrollToBottom();
+  }
+};
+
+socket.on("connect", function() {
+  console.log("Connected");
+
+  const params = {
+    channelID,
+    userID
+  };
+
+  socket.emit("join", params, function(err) {
+    if (err) {
+      alert(err);
+      window.location.href = "/";
+    } else {
+      console.log("No Error");
+    }
+  });
+});
+
+function renderError (msg)
+{
+  const div = jQuery("<div class='chat-message'></div>");
+    div.html(`<div class="chat-message-content">
+      <a href="#">System</div>
+      <div class="chat-message-message">
+        <p style="color: red !important">${msg}</p>
+      </div>
+    `);
+    jQuery("#mCSB_2_container").append(div);
+    scrollToBottom();
+}
+
+jQuery("#message-form").on("submit", function(e) {
+  e.preventDefault();
+
+  var messageTextBox = jQuery("[name=message]");
+
+  var message = messageTextBox.val().trim();
+
+  if (message === "") return;
+  
+  if (message.startsWith("/"))
+  {
+    const args = message.substring(1, message.length).trim().split(/\s/g);
+    const command = args.shift();
+    if (!command) return renderError("Missing command input!");
+    if (!commands[command]) return renderError("Command not found!");
+    commands[command](args);
+    return;
+  }
+  
+  var data = {
+    userID,
+    channelID: channelID,
+    message
+  };
+
+  socket.emit("createdMessage", data, function() {
+    messageTextBox.val(" ");
+  });
+});
+
+socket.on("newMessage", function(message) {
+  const formatedTime = moment(message.created_at).format("lll");
+
+  const div = jQuery("<div class='chat-message'></div>");
+  div.html(`
             <div class="avatar"><img src="${message.author.profile_picture}" /></div>
             <div class="chat-message-content">
                 <a href="#" class="chat-message-author">${message.author.username}</a>
@@ -61,56 +94,64 @@ socket.on("newMessage", function(message){
             </div>
     `);
 
-    jQuery("#mCSB_2_container").append(div);
-    scrollToBottom();
+  jQuery("#mCSB_2_container").append(div);
+  scrollToBottom();
 });
 
-socket.on("disconnect", function(){
-    console.log("Disconnected to server");
+socket.on("disconnect", function() {
+  console.log("Disconnected to server");
 });
-
-
 
 // if User is looking at previous message we should not scroll down -- not yet implemented
-function scrollToBottom(){
-    // // Selectors
-     var element = jQuery(".scroll-hijack");
-     var messagesContainer = jQuery("#mCSB_2_container");
-     var chatbody = jQuery("#mCSB_2");
-     var newMessage = messagesContainer.children().last();
+function scrollToBottom() {
+  // // Selectors
+  var element = jQuery(".scroll-hijack");
+  var messagesContainer = jQuery("#mCSB_2_container");
+  var chatbody = jQuery("#mCSB_2");
+  var newMessage = messagesContainer.children().last();
 
-    // // // Heights
-     var clientHeight = messagesContainer.prop("clientHeight");
-     var scrollTop = messagesContainer.prop("scrollTop");
-     var scrollHeight = messagesContainer.prop("scrollHeight");
-     var newMessageHeight = newMessage.innerHeight();
-     var lastMessageHeight = newMessage.prev().innerHeight();
+  // // // Heights
+  var clientHeight = messagesContainer.prop("clientHeight");
+  var scrollTop = messagesContainer.prop("scrollTop");
+  var scrollHeight = messagesContainer.prop("scrollHeight");
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
 
-     console.log(clientHeight, scrollTop, scrollHeight, newMessageHeight, lastMessageHeight);
+  console.log(
+    clientHeight,
+    scrollTop,
+    scrollHeight,
+    newMessageHeight,
+    lastMessageHeight
+  );
 
-     if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
-         console.log("scroll");
-        element.mCustomScrollbar("scrollTo","bottom");
-     }
-
-
+  if (
+    clientHeight + scrollTop + newMessageHeight + lastMessageHeight >=
+    scrollHeight
+  ) {
+    console.log("scroll");
+    element.mCustomScrollbar("scrollTo", "bottom");
+  }
 }
 
-(function fetchOnlineUser(){
-    $.get("/current/channel/"+ channelID )
-        .done(function(data){
-            chatList.html("");
-            data.forEach(function(participant){
-                const randomNumber = Math.floor((Math.random() * 7) + 1);
-                const pUsername = participant.username;
-                if(pUsername !== username["0"].text){
-                    if(participant.online === true){
-                        chatList.append(`<li><a href="/users/${participant.id}" class="user"><div class="avatar"><img class="little_avatar"  src="${participant.image}" /></div>${pUsername}</a></li>`);
-                    }else{
-                         chatList.append(`<li><a href="/users/${participant.id}"><div class="avatar"><img class="little_avatar"  src="${participant.image}" /></div>${pUsername}</a></li>`);
-                    }
-                }
-            });
-        });
-    setTimeout(fetchOnlineUser,30000);
-}())
+(function fetchOnlineUser() {
+  $.get("/current/channel/" + channelID).done(function(data) {
+    chatList.html("");
+    data.forEach(function(participant) {
+      const randomNumber = Math.floor(Math.random() * 7 + 1);
+      const pUsername = participant.username;
+      if (pUsername !== username["0"].text) {
+        if (participant.online === true) {
+          chatList.append(
+            `<li><a href="/users/${participant.id}" class="user"><div class="avatar"><img class="little_avatar"  src="${participant.image}" /></div>${pUsername}</a></li>`
+          );
+        } else {
+          chatList.append(
+            `<li><a href="/users/${participant.id}"><div class="avatar"><img class="little_avatar"  src="${participant.image}" /></div>${pUsername}</a></li>`
+          );
+        }
+      }
+    });
+  });
+  setTimeout(fetchOnlineUser, 30000);
+})();
