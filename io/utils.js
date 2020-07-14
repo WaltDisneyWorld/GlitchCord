cce48f7830172ae32ddf00015b4ecf0bae15f8ef
Glitch (glitchchord) <none>
2020-07-14 16:54:47 +0000

    🗽⚽️ Checkpoint ./views/chat.ejs:5152800/137 ./io/utils.js:5152800/61 ./package.json:5152800/2776

--- io/utils.js
+++ io/utils.js
@@ -4,7 +4,7 @@ const Message = require("../models/message");
 const Channel = require("../models/channel");
 const socket = require("socket.io");
 const Filter = require("bad-words");
-
+var md   = require('markdown');
 const filter = new Filter();
 
 const utils = {};
@@ -13,7 +13,7 @@ utils.saveMessage = function saveMessage(io, data) {
   User.findById(ObjectID(data.userID))
     .then(rUser => {
       const msg = {
-        text: filter.clean(data.message.toString().safe()),
+        text: md.parse(filter.clean(data.message.toString().safe())),
         author: rUser
       };
       Message.create(msg)
